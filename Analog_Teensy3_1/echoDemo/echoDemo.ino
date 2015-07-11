a6756f4d312d9f82fa9165123b51d4c554232d73
hyz <zhao_hanyang@hotmail.com>
2015-07-11 17:57:57 -0400

    update app

--- Analog_Teensy3_1/echoDemo/echoDemo.ino
+++ Analog_Teensy3_1/echoDemo/echoDemo.ino
@@ -18,11 +18,23 @@ All text above, and the splash screen below must be included in any redistributi
 #include <SPI.h>
 #include <Adafruit_BLE_UART.h>
 #include <Snooze.h>
+#include <Metro.h> 
+
 
 
 /*** Must be global ***/
+
+float tempNow = 0;
+float tempSensor = 0;
+int isNegative = 0;
+int readIsNegative = 0;
+String s;
 SnoozeBlock config;
-float tempPast = 0;
+int tempOptimal = 0;
+int temptooLow = tempOptimal - 50 ;
+int temptooHigh = tempOptimal + 50;
+Metro serialMetro = Metro(10000);
+char readBuffer[3];
 // Connect CLK/MISO/MOSI to hardware SPI
 // e.g. On UNO & compatible: CLK = 13, MISO = 12, MOSI = 11
 #define ADAFRUITBLE_REQ 10
@@ -31,6 +43,7 @@ float tempPast = 0;
 
 Adafruit_BLE_UART BTLEserial = Adafruit_BLE_UART(ADAFRUITBLE_REQ, ADAFRUITBLE_RDY, ADAFRUITBLE_RST);
 aci_evt_opcode_t status;
+
 /**************************************************************************/
 /*!
     Configure the Arduino and start advertising with the radio
@@ -39,26 +52,28 @@ aci_evt_opcode_t status;
 void setup(void)
 { 
   
-  Serial.begin(38400);
-  while(!Serial); // Leonardo/Micro should wait for serial init
-  Serial.println(F("Adafruit Bluefruit Low Energy nRF8001 Print echo demo"));
+  Serial1.begin(9600);
+  //while(!Serial); // Leonardo/Micro should wait for serial init
+  Serial1.println(F("Adafruit Bluefruit Low Energy nRF8001 Print echo demo"));
 
   BTLEserial.setDeviceName("BBQTemp"); /* 7 characters max! */
 
-  //pinMode(ADAFRUITBLE_RDY, INPUT_PULLUP);
-  //config.pinMode(ADAFRUITBLE_RDY,INPUT_PULLUP,RISING);
+  pinMode(3,INPUT);
+  config.pinMode(3,INPUT_PULLUP,RISING);
   BTLEserial.begin();
-  //config.setTimer(10000);
+  //config.setTimer(5000);
 
+  // //Get the radio advertising before we sleep
+  while (status != ACI_EVT_DEVICE_STARTED) {
+   status = BTLEserial.getState();
+   BTLEserial.pollACI();
+   
+  }
+  Serial1.println("BLE started");
+  tempOptimal = analogRead(A0) * (3.3 / 1023.0);
+  tempOptimal = ((tempOptimal - 1.25) / 0.005 ) ;
   
-  //Get the radio advertising before we sleep
-  // while (status != ACI_EVT_DEVICE_STARTED) {
-  //  status = BTLEserial.getState();
-  //  BTLEserial.pollACI();
-  //  Serial.println("BLE started");
-  //}
-  
- }
+}
 
 /**************************************************************************/
 /*!
@@ -70,26 +85,9 @@ aci_evt_opcode_t laststatus = ACI_EVT_DISCONNECTED;
 void loop()
 { 
 
-  //int tempMinimum = 200;
-  //int tempOptimal = 275;
-  //int tempMax = 350;
-  //int temptooLow = 0;
-  //int temptooHigh = 0;
   //int tempJustRight = 0; 
-  int isNegative = 0;
+  isNegative = 0;
 
-  float tempSensor = analogRead(A0) * (3.3 / 1023.0);
-  float tempNow = ( (tempSensor - 1.25) / 0.005 ) ;
-  if(tempNow < 0){
-    isNegative = 1;
-    tempNow = tempNow * - 1.0;
-  }
-  // if (digitalRead(ADAFRUITBLE_RDY) == HIGH) {
-  //   Serial.println("SLEEP");
-  //   Serial.flush();
-  //   Snooze.sleep(config);
-  //   Serial.println("WAKE");
-  // }
   // Tell the nRF8001 to do whatever it should be working on.
   BTLEserial.pollACI();
 
@@ -99,13 +97,31 @@ void loop()
   if (status != laststatus) {
     // print it out!s
     if (status == ACI_EVT_DEVICE_STARTED) {
-      Serial.println(F("* Advertising started"));
+      Serial1.println(F("* Advertising started"));
+      if (digitalRead(3) == HIGH) {
+      Serial1.println("SLEEP_started");
+      Serial1.flush();
+      Snooze.sleep(config);
+      Serial1.println("WAKE_started");
+      }
+  
+      //Serial.println(F("*sleeping"));
+      //Snooze.sleep(config);
+      //Serial.println(F("awake"));
+
     }
     if (status == ACI_EVT_CONNECTED) {
-      Serial.println(F("* Connected!"));
+      Serial1.println(F("* Connected!"));
     }
     if (status == ACI_EVT_DISCONNECTED) {
-      Serial.println(F("* Disconnected or advertising timed out"));
+      Serial1.println(F("* Disconnected or advertising timed out"));
+      
+     // if (digitalRead(3) == HIGH) {
+      // Serial1.println("SLEEP_Disconnected");
+      // Serial1.flush();
+      // Snooze.sleep(config);
+      // Serial1.println("WAKE_disconnected");
+    //}
     }
     // OK set the last status change to this one
     laststatus = status;
@@ -113,33 +129,47 @@ void loop()
 
   if (status == ACI_EVT_CONNECTED) {
     // Lets see if there's any data for us!
-    if (BTLEserial.available()) {
-      //Serial.print("* "); Serial.print(BTLEserial.available()); Serial.println(F(" bytes available from BTLE"));
-    }
     // OK while we still have something to read, get a character and print it out
+    int i = 0;
     while (BTLEserial.available()) {
       char c = BTLEserial.read();
-      Serial.print(c);
+      if(c = '-'){
+         readIsNegative = 1;     
+      }
+      else{
+        readBuffer[i] = c;
+        i++;
+      }
     }
 
-    if(abs(tempNow - tempPast) > 2.0 ){
+    if(readIsNegative){
+      tempOptimal = -1 * ( (readBuffer[0] - '0') * 100 + ( readBuffer[1] - '0') * 10 + ( readBuffer[2] - '0') );
+    }
+    else{
+      tempOptimal = ( (readBuffer[0] - '0') * 100 + ( readBuffer[1] - '0') * 10 + ( readBuffer[2] - '0') );
+    }
+
+    //Serial.print(tempOptimal);
+    if (serialMetro.check() == 1) { 
+      tempSensor = analogRead(A0);
+      tempSensor = analogRead(A0) * (3.3 / 1023.0);
+      tempNow = ( (tempSensor - 1.25) / 0.005 ) ;
+      if(tempNow < 0){
+        isNegative = 1;
+        tempNow = tempNow * - 1.0;
+      }
+
       char buffer[5];
       unsigned char sendbuffer[10];
-      String s = dtostrf(tempNow,5,2,buffer);
+      s = dtostrf(tempNow,5,2,buffer);
       if(isNegative){
         s = "-" + s;
       }
       s.getBytes(sendbuffer, 5);
       char sendbuffersize = min(5, s.length());
       // write the data
-      BTLEserial.write(sendbuffer, sendbuffersize);
-      //Snooze.sleep(config);
-      // digitalWrite(LED_BUILTIN, HIGH);
-      // delay(1000);
-      // digitalWrite(LED_BUILTIN, LOW);
-      tempPast = tempNow;
-    }
-
     
+      BTLEserial.write(sendbuffer, sendbuffersize);
+    }
   }
 }
